e09bf4b2ee170079afcadd82229c851babd61c16
Kaskeeeee <matteo.cavaliere1@gmail.com>
2020-05-19 11:53:55 +0200

    Modified main and login controller, added icons folder

--- epidemiological_investigations/src/epidemic/controller/LoginController.java
+++ epidemiological_investigations/src/epidemic/controller/LoginController.java
@@ -1,5 +1,23 @@
 package epidemic.controller;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
+public class LoginController {
+	private MainClass mainReference;
+	
+	public void setMainReference(MainClass mainReference) {
+		this.mainReference = mainReference;
+	}
+	
+	@FXML
+	public void handleLogin(ActionEvent e) {
+		Alert alert = new Alert(AlertType.WARNING);
+        alert.initOwner(mainReference.getPrimaryStage());
+        alert.setTitle("Authentication problem");
+        alert.setHeaderText("You failed to login. Try again!");
+        alert.showAndWait();
+	}
 }
--- epidemiological_investigations/src/epidemic/controller/MainClass.java
+++ epidemiological_investigations/src/epidemic/controller/MainClass.java
@@ -1,31 +1,83 @@
 package epidemic.controller;
 
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 public class MainClass extends Application{
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		primaryStage.setTitle("Log in");
+		
+		initRootLayout();
+		showLoginScreen();
+	}
 
+	/**
+	 * Initialize the root
+	 */
+	public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainClass.class.getResource("/epidemic/view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public void showLoginScreen() {
+        try {
+            // Load login screen.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainClass.class.getResource("/epidemic/view/Login.fxml"));
+            VBox LoginScreen = (VBox)loader.load();
+            
+            // Set login screen into the center of root layout.
+            rootLayout.setCenter(LoginScreen);
+            
+            //Set stage not resizable
+            primaryStage.setResizable(false);
+            
+            //set reference to main class
+            LoginController controller = loader.getController();
+            controller.setMainReference(this);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	
+	/**
+	 * @return primaryStage
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+	
+	/**
+	 * main method
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/epidemic/view/Login.fxml"));
-			Scene scene = new Scene(root);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Log in");
-			primaryStage.setResizable(false);
-			primaryStage.show();
-		} catch(Exception e){
-			e.printStackTrace();
-		}
-		
-	}
+	
 
 }
