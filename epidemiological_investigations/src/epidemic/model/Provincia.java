3154662a0feb513d85ac9e84f3dae6aa44b270c0
Kaskeeeee <matteo.cavaliere1@gmail.com>
2020-05-24 12:29:26 +0200

    modificato classi regione e provincia per rispettare relazione 1:N

--- epidemiological_investigations/src/epidemic/model/Provincia.java
+++ epidemiological_investigations/src/epidemic/model/Provincia.java
@@ -2,14 +2,20 @@ package epidemic.model;
 
 public class Provincia extends Localita {
 	private final Comune capoluogo;
+	private final Regione regioneAppartenenza;
 	
-	public Provincia(String nome, float superficie, Comune capoluogo) {
+	public Provincia(String nome, float superficie, Comune capoluogo, Regione regioneAppartenenza) {
 		super(nome, superficie);
 		this.capoluogo = capoluogo;
+		this.regioneAppartenenza = regioneAppartenenza;
 	}
 
 	public Comune getCapoluogo() {
 		return capoluogo;
+	}
+	
+	public Regione getRegioneAppartenenza() {
+		return regioneAppartenenza;
 	}	
 	
 }
--- epidemiological_investigations/src/epidemic/model/Regione.java
+++ epidemiological_investigations/src/epidemic/model/Regione.java
@@ -1,23 +1,15 @@
 package epidemic.model;
 
-import java.util.List;
-
 public class Regione extends Localita{
 	private Comune capoluogo;
-	private List<Provincia> province;
 	
-	public Regione(String nome, float superficie, Comune capoluogo, List<Provincia> province) {
+	public Regione(String nome, float superficie, Comune capoluogo) {
 		super(nome, superficie);
 		this.capoluogo = capoluogo;
-		this.province = province;
 	}
 	
 	public Comune getCapoluogo() {
 		return capoluogo;
-	}
-	
-	public List<Provincia> getProvince(){
-		return province;
 	}
 
 }
